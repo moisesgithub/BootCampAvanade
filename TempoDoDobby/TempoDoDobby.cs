69ea1785c215b8b274ea54aad4f7925c46f941e6
moisesgithub <moises.github@outlook.com>
2021-08-30 17:11:48 -0300

    TempoDoDobby

---
+++ TempoDoDobby/TempoDoDobby.cs
@@ -0,0 +1,51 @@
+﻿using System;
+class MinhaClasse
+{
+    public static void Main(string[] args)
+    {
+        string[] line1 = Console.ReadLine().Split(" ");
+        string[] line2 = Console.ReadLine().Split(" ");
+
+        int finalExpediente = int.Parse(line1[0]);
+
+        int presente1 = int.Parse(line2[0]);
+        int presente2 = int.Parse(line2[1]);
+
+        int totalNecessario = presente1 + presente2;
+
+        if (totalNecessario > finalExpediente)
+        {
+            Console.WriteLine("Deixa para amanha!");
+        }
+        else
+        {
+            Console.WriteLine("Farei hoje!");
+        }
+    }
+}
+
+/*using System;
+
+class MinhaClasse {
+  public static void Main (string[] args) {
+    string[] line1 = Console.ReadLine().Split(" ");
+    string[] line2 = Console.ReadLine().Split(" ");
+    
+    int finalExpediente = int.Parse(line1[0]);
+    
+    int presente1 = int.Parse(line2[0]);
+    int presente2 = int.Parse(line2[1]);
+    
+     //continue a solucao
+    int totalNecessario =  
+    
+    if (                                 )
+    {
+      Console.WriteLine("Deixa para amanha!");
+    }
+    else
+    {
+      Console.WriteLine("Farei hoje!");
+    }
+  }
+}*/
