78508d4f706366c9aad2de9f870f6b288fe350b7
moisesgithub <moises.github@outlook.com>
2021-08-30 17:01:45 -0300

    ConversaoDeTempo

---
+++ ConversaoDeTempo/ConversaoDeTempo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+class MinhaClasse
+{
+    static void Main(string[] args)
+    {
+        var timeInSeconds = int.Parse(Console.ReadLine());
+
+        var hours = timeInSeconds / (60 * 60);
+        timeInSeconds = timeInSeconds % (60 * 60);
+        var minutes = timeInSeconds / 60;
+        var seconds = timeInSeconds % 60;
+
+        Console.WriteLine($"{hours}:{minutes}:{seconds}");
+    }
+}
+
+/*using System;
+
+    class MinhaClasse {
+        static void Main(string[] args) {
+            var timeInSeconds = int.Parse(Console.ReadLine());
+
+       //continue a solucao
+            var hours = ;
+            timeInSeconds =  ;
+            var minutes =   ;
+            var seconds =    ;
+
+            Console.WriteLine($"{hours}:{minutes}:{seconds}");
+        }
+    }*/
