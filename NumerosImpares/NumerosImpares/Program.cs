a223deba0209599e6dffcbe3e6760bf2d984b5b5
moisesgithub <moises.github@outlook.com>
2021-08-30 16:51:39 -0300

    Introdução a Resolução de Desafios com C#

---
+++ NumerosImpares/NumerosImpares/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+class MinhaClasse
+{
+    public static void Main()
+    {
+        int n;
+        n = int.Parse(Console.ReadLine());
+        for (int i = 1; i <= n; i += 2)
+        {
+            Console.WriteLine(i);
+        }
+        Console.ReadLine();
+    }
+}
+
+/*using System; 
+
+class minhaClasse {
+
+        public static void Main()
+        {
+            int n;
+            n = int.Parse(Console.ReadLine());
+            //complete seu codigo
+            for (              ) //complete seu codigo
+            {
+                Console.WriteLine( );
+            }
+            Console.ReadLine();
+        }
+}*/
