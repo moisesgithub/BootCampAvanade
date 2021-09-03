558f96b140a0c662c13bf7a1f68e43e1f35c5e7e
moisesgithub <moises.github@outlook.com>
2021-09-03 11:23:18 -0300

    O Maior

---
+++ OMaior/OMaior/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+class Program
+{    
+    static void Main(string[] args)
+    {        
+        string[] vet = Console.ReadLine().Split(' ');
+
+        int MAIORAB = (int.Parse(vet[0]) + int.Parse(vet[1]) 
+                    + Math.Abs(int.Parse(vet[0]) 
+                    - int.Parse(vet[1]))) / 2;
+
+        int MAIOR = (MAIORAB + int.Parse(vet[2]) 
+                  + Math.Abs(MAIORAB 
+                  - int.Parse(vet[2]))) / 2;
+
+        Console.WriteLine($"{MAIOR} eh o maior");
+    }    
+}
+
+/*using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vet = Console.ReadLine().Split(' ');
+            //complete declarando as variáveis
+
+            int MAIORAB = (                )/2;
+
+            int MAIOR = (                ) / 2;
+
+            Console.WriteLine($"{MAIOR} eh o maior");
+                        
+        }
+    }*/
